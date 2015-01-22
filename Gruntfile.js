243f51282abff0f256055cdf3f74c32525649edc
Matthias Kretschmann <m@kretschmann.io>
2015-01-22 15:12:39 +0100

    drop da base

---
+++ Gruntfile.js
@@ -0,0 +1,157 @@
+module.exports = function(grunt) {
+
+    // banner
+    grunt.log.writeln("");
+    grunt.log.writeln("   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    grunt.log.writeln("");
+    grunt.log.writeln("      (o) Just what do you think you're doing, Matthias?    ");
+    grunt.log.writeln("");
+    grunt.log.writeln("   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    grunt.log.writeln("");
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Clean stuff
+        clean: ['dist/**'],
+
+        // Copy assets around
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'src/',
+                    dest: 'dist/',
+                    src: [
+                        'assets/**',
+                        '!assets/styl/**'
+                    ]
+                }]
+            }
+        },
+
+        // Stylus
+        stylus: {
+            dist: {
+                options: {
+                    'include css': true
+                },
+                files: {
+                    'dist/assets/css/plumage.css': 'src/assets/styl/plumage.styl'
+                }
+            }
+        },
+
+        // CSS minification
+        cssmin: {
+            dist: {
+                options: {
+                    banner: '/* plumage landing page | Author: Matthias Kretschmann <m@kretschmann.io> | © 2015 plumage */'
+                },
+                files: {
+                    'dist/assets/css/plumage.css': ['dist/assets/css/plumage.css']
+                }
+            }
+        },
+
+        // JS concatenation & minification
+        uglify: {
+            dist: {
+                files: {
+                    'dist/assets/js/plumage.min.js': ['src/assets/js/plumage.js'],
+                    'dist/assets/js/jquery.min.js': ['bower_components/jquery/dist/jquery.js']
+                }
+            }
+        },
+
+        // Assembles html layout
+        assemble: {
+            options: {
+                layoutdir: 'src/layouts',
+                flatten: true
+            },
+            pages: {
+                src: ['src/screens/*.hbs'],
+                dest: 'dist/'
+            }
+        },
+
+        // Watches for changes to css or email templates then runs grunt tasks
+        watch: {
+            options: {
+                livereload: true
+            },
+            stylus: {
+                files: ['src/**/*.styl'],
+                tasks: ['stylus']
+            },
+            js: {
+                files: ['src/**/*.js'],
+                tasks: ['uglify']
+            },
+            templates: {
+                files: ['src/**/*.hbs'],
+                tasks: ['assemble']
+            },
+            images: {
+                files: ['src/assets/img/**'],
+                tasks: ['default']
+            }
+        },
+
+        // dev server
+        connect: {
+            server: {
+                options: {
+                    port: 1337,
+                    hostname: '*',
+                    base: 'dist',
+                    open: {
+                         target: 'http://localhost:1337'
+                    }
+                }
+            }
+        },
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('assemble');
+
+    // Default task
+    grunt.registerTask('default', [
+        'copy',
+        'stylus',
+        'uglify',
+        'assemble'
+    ]);
+
+    // Dev server
+    grunt.registerTask('server', [
+        'clean',
+        'copy',
+        'stylus',
+        'uglify',
+        'assemble',
+        'connect',
+        'watch'
+    ]);
+
+    // Final build
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'stylus',
+        'cssmin',
+        'uglify',
+        'assemble'
+    ]);
+
+};
---
+++ README.md
@@ -0,0 +1,52 @@
+# plumage landing page
+
+> Landing page for plumage. Splendid.
+
+This repository does the following:
+
+1. compiles Stylus files into CSS and minifies it
+2. concatenates and minifies JavaScript files
+3. builds templates based on [assemble](http://assemble.io) layout templates
+4. spins up static web server for development
+
+## Browser support
+
+Generally, templates are supported in the current and last 2 versions of all modern browsers. Specifically, they're supposed to work (but not look identical) and tested in the following browsers which aligns with ezeep's web app browser compatibility:
+
+- *Desktop*: IE 9+, Chrome 27+, Firefox 24+, Safari 5+, Opera 12+
+- *Mobile*: Mobile Safari 6+, Chrome for Android/iOS 26+, Android Browser 4.1+
+
+## Prerequisites
+
+- [node.js](http://nodejs.org/) & [npm](https://npmjs.org/)
+- [Bower](http://bower.io/)
+
+
+To get all dependencies run:
+```bash
+npm install
+```
+
+This will install all required npm packages and bower components in one go.
+
+
+## Development
+
+This compiles and builds everything into the `dist/` folder and starts up a watching & live-reloading web server:
+
+```bash
+grunt server
+```
+
+The site will open [localhost:1337](http://localhost:1337) automatically in your default browser.
+
+Note: The easiest way to get livereload to work is to install the [browser extension](http://feedback.livereload.com/knowledgebase/articles/86242-how-do-i-install-and-use-the-browser-extensions-).
+
+
+## Build
+
+This will put everything together and output it in the `dist/` folder:
+
+```bash
+grunt build
+```
