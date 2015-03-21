9d65f915e14fb66ce3aab86f13f900be2a6463fe
Matthias Kretschmann <m@kretschmann.io>
2015-03-21 21:23:12 +0100

    content updates, this and that

--- dist/assets/js/plumage.js
+++ dist/assets/js/plumage.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
 
+    //
+    // Smooth scrolling
+    //
+    var $scrollLinks = $('a.scroll');
+
+    $scrollLinks.click(function(e) {
+        e.preventDefault();
+
+		var target 	        = this.hash,
+			$target         = $(target),
+            targetOffset    = $target.offset().top;
+
+        $('html, body').stop().animate({
+			scrollTop: targetOffset
+		}, 500);
+    });
+
 
 });
