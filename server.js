4eefca3716a780b270883d3a1666a7d4897ad79a
Tom Houlé <tom@tomhoule.com>
2019-04-24 22:21:07 +0200

    ♻️ Listen to the port passed by heroku (or 3000)

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var express = require('express'),
     path = require('path'),
     app = express();
 
+var port = process.env.PORT || 3000
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 console.log("starting the nyapp on http://localhost:3000")
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(port);
